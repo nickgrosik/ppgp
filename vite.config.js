818176e279df0bf806d21f638df81088454868de
nickgrosik <nickgrosik@gmail.com>
2026-04-07 22:48:41 -0400

    Spring cleaning

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
